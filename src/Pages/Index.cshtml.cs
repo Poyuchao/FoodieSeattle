8537fc3e6903e17a531011346b70d19f8ce7eb2a
BuBuCatt <79895060+BuBuCatt@users.noreply.github.com>
2023-04-10 19:22:21 -0700

    Added name ChiaYu Hsu to indexmodel comment in index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -8,7 +8,9 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-  
+  /// <summary>
+  /// ChiaYu Hsu
+  /// </summary>
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
