41bd3e3960e12b0475dc02e9ebfe76268ffb8d6e
BubuCatt <jiayuhsu8240@gmail.com>
2023-04-22 01:52:09 -0700

    ADD CRUDi -Read
    click on the "Read" button located in the corresponding row of the table. This will trigger a pop-up window displaying the detailed description of the restaurant.

---
+++ src/Pages/Read.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class ReadModel : PageModel
+    {
+
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public ReadModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show
+        public ProductModel Product;
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+
+
+
+
+        public void OnGet(string id) => Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+    }
+}
