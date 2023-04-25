6a4779d9c3ead97ac329fed42252c68f5a0baedc
BubuCatt <jiayuhsu8240@gmail.com>
2023-04-24 17:09:04 -0700

    Update Program.cs
    updated comment for each function

--- src/Program.cs
+++ src/Program.cs
@@ -5,11 +5,14 @@ namespace ContosoCrafts.WebSite
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) //This is the Main method for the Program class. It takes an array of strings named args as a parameter.
+                                               //It creates an instance of a host using the CreateHostBuilder method, builds the host, and runs it.
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+        //This is the CreateHostBuilder method for the Program class. It takes an array of strings named args as a parameter. It creates a default host builder using Host.CreateDefaultBuilder,
+        //and then configures the web host using webBuilder.UseStartup<Startup>(),
+        //which sets the startup class for the application to Startup.
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
