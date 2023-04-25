9945249ea68fca42541845f56bfded4d1d37e6a6
BubuCatt <jiayuhsu8240@gmail.com>
2023-04-24 17:08:59 -0700

    Update Read.cshtml.cs
    updated comment for each function

--- src/Pages/Read.cshtml.cs
+++ src/Pages/Read.cshtml.cs
@@ -1,3 +1,5 @@
+
+//These lines import the necessary namespaces for this class.
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    //This line defines a new class named ReadModel that inherits from the PageModel class.
     public class ReadModel : PageModel
     {
-
+        //This line creates a public property named ProductService of type JsonFileProductService. This property will be used to retrieve the products to display.
         public JsonFileProductService ProductService { get; }
 
         /// <summary>
@@ -19,12 +22,14 @@ namespace ContosoCrafts.WebSite.Pages
         /// </summary>
         /// <param name="logger"></param>
         /// <param name="productService"></param>
+        /// //This is the constructor for the ReadModel class. It takes an instance of JsonFileProductService as a parameter and assigns it to the ProductService property.
         public ReadModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
         // The data to show
+        //This is a public property of type ProductModel named Product. This property will hold the data to display on the page.
         public ProductModel Product;
 
         /// <summary>
@@ -34,7 +39,9 @@ namespace ContosoCrafts.WebSite.Pages
 
 
 
-
+        //This is the OnGet method for the ReadModel class. It takes an id parameter and uses it to retrieve a single product from the JsonFileProductService.
+        //It then assigns the product to the Product property for display on the page.
+        //This method uses LINQ to filter the list of products to find the one with the matching id
         public void OnGet(string id) => Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
     }
 }
