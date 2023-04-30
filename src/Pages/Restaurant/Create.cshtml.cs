0abf7d55a2fea19a07bbe13f17cc8d7ad6ddcafe
benccooper <bcooper@seattleu.edu>
2023-04-30 12:28:04 -0700

    Added Create.cshtml.cs to Pages/Restaurant

---
+++ src/Pages/Restaurant/Create.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Restaurant
+{
+    public class CreateModel : PageModel
+    {
+        // Data middle tier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public CreateModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show
+        public ProductModel Product;
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public IActionResult OnGet()
+        {
+            Product = ProductService.CreateData();
+
+            return RedirectToPage("./Update", new { Id = Product.Id });
+        }
+    }
+}
