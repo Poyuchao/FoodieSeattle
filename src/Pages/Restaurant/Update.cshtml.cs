d0cf73849b131d440756cf1dbdfdd50d846ec0b1
benccooper <bcooper@seattleu.edu>
2023-04-30 12:30:55 -0700

    Added Update.cshtml.cs (Model file) to Pages/Restaurant

---
+++ src/Pages/Restaurant/Update.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+
+namespace ContosoCrafts.WebSite.Pages.Restaurant
+{
+    /// <summary>
+    /// Manage the Update of the data for a single record
+    /// </summary>
+
+    public class UpdateModel : PageModel
+    {
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public UpdateModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// Loads the Data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Post the model back to the page
+        /// The model is in the class variable Product
+        /// Call the data layer to Update that data
+        /// Then return to the index page
+        /// </summary>
+        /// <returns></returns>
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ProductService.UpdateData(Product);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
