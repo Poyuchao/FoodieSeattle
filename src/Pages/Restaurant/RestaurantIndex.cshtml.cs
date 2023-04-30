cf1d70c6101be8fb8273b8a0eee40928b97c5fe5
benccooper <bcooper@seattleu.edu>
2023-04-30 12:29:12 -0700

    Added RestaurantIndex/cshtml.cs to Pages/Restaurant

---
+++ src/Pages/Restaurant/RestaurantIndex.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Restaurant
+{
+    public class RestaurantIndexModel : PageModel
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public RestaurantIndexModel (JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet
+        /// Return all the data
+        /// </summary>
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
