9e3a475fc1af628c54bf9da5c11704b6a52249d4
benccooper <bcooper@seattleu.edu>
2023-05-01 14:29:26 -0700

    Added Create.cshtml.Tests.cs base code file under UnitTests/Pages/Restaurant

---
+++ UnitTests/Pages/Restaurant/Create.cshtml.Tests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Restaurant;
+
+
+namespace UnitTests.Pages.Product.Create
+{
+    public class CreateTests
+    {
+        #region TestSetup
+        public static CreateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new CreateModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+            var oldCount = TestHelper.ProductService.GetProducts().Count();
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(oldCount + 1, TestHelper.ProductService.GetProducts().Count());
+        }
+        #endregion OnGet
+    }
+}
