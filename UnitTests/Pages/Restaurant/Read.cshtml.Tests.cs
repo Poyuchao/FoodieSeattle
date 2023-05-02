cfe1233fb0b7198b01b59a438f54e93d2e383296
BuBuCatt <79895060+BuBuCatt@users.noreply.github.com>
2023-05-01 23:44:26 -0700

    Create Read page Unit Test

---
+++ UnitTests/Pages/Restaurant/Read.cshtml.Tests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Services;
+using Moq;
+using ContosoCrafts.WebSite.Pages.Restaurant;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace UnitTests.Pages.Restaurant.Read
+{
+
+    /// <summary>
+    /// Unit tests for Read page
+    /// </summary>
+
+    public class ReadTests
+    {
+        private ReadModel callReadModel; 
+
+        private List<ProductModel> expectedProducts;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Create ReadModel instance and expected product list
+            callReadModel = new ReadModel(TestHelper.ProductService);
+            expectedProducts = TestHelper.ProductService.GetProducts().ToList();
+        }
+
+        [Test]
+
+        public void OnGet_Retuen_Same_Data()
+        {
+            //arrange
+
+            var expectedProduct = TestHelper.ProductService.GetProducts().First().Title;
+
+            //Act
+             callReadModel.OnGet("kashiba-pic");
+            var result = callReadModel.Product.Title;
+
+            //Assert
+
+            Assert.AreEqual(expectedProduct, result);
+
+            
+        }
+
+
+
+    }
+}
