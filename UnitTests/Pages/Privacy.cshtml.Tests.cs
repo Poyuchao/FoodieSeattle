9469951ad275ada3e20b6b4efe4e1dba466c876f
benccooper <bcooper@seattleu.edu>
2023-05-02 15:29:41 -0700

    Commented out code in Privacy.cshtml.Tests.cs until file gets updated later on.

--- UnitTests/Pages/Privacy.cshtml.Tests.cs
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -1,18 +1,18 @@
-using NUnit.Framework;
+//using NUnit.Framework;
 
-namespace UnitTests.Pages
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+//namespace UnitTests.Pages
+//{
+//    public class Tests
+//    {
+//        [SetUp]
+//        public void Setup()
+//        {
+//        }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
-}
+//        [Test]
+//        public void Test1()
+//        {
+//            Assert.Pass();
+//        }
+//    }
+//}
