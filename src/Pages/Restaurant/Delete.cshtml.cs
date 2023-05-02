213fa42a360484b7ca6870b86ab391c2fecfdb5a
BuBuCatt <79895060+BuBuCatt@users.noreply.github.com>
2023-05-02 00:15:36 -0700

    Create a 'Delete' CRUDi page under Pages/Restaurant.

---
+++ src/Pages/Restaurant/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+
+namespace ContosoCrafts.WebSite.Pages.Restaurant
+{/// <summary>
+ /// Manage the Delete of the data for a single record
+ /// </summary>
+    public class DeleteModel : PageModel
+    {
+        // Data middletier
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public DeleteModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// Loads the Data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Post the model back to the page
+        /// The model is in the class variable Product
+        /// Call the data layer to Delete that data
+        /// Then return to the index page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ProductService.DeleteData(Product.Id);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -132,5 +132,23 @@ namespace ContosoCrafts.WebSite.Services
 
             return data;
         }
+
+
+        /// <summary>
+        /// Remove the item from the system
+        /// </summary>
+        /// <returns></returns>
+        public ProductModel DeleteData(string id)
+        {
+            // Get the current set, and append the new record to it
+            var dataSet = GetProducts();
+            var data = dataSet.FirstOrDefault(m => m.Id.Equals(id));
+
+            var newDataSet = GetProducts().Where(m => m.Id.Equals(id) == false);
+
+            SaveData(newDataSet);
+
+            return data;
+        }
     }
 }
