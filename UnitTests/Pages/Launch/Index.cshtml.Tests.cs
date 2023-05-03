f911d2d43007a54480f8de0775f6ba39252de75e
benccooper <bcooper@seattleu.edu>
2023-05-03 11:43:16 -0700

    Added placeholder UT file Index.cshtml.Tests.cs under UnitTests/Pages/Launch

---
+++ UnitTests/Pages/Launch/Index.cshtml.Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace UnitTests.Pages.Launch
+{
+	/// <summary>
+	/// Unit testing for IndexTests
+	/// </summary>
+	public class IndexTests
+	{
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public IndexTests()
+		{
+		}
+	}
+}
+
