2de6b5b2d4083f9a28aac18f6cc6213726caf801
benccooper <bcooper@seattleu.edu>
2023-05-03 08:38:28 -0700

    Updated base code and documentation in Privacy.cshtml.Tests.cs

--- UnitTests/Pages/Privacy.cshtml.Tests.cs
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -1,18 +1,59 @@
-//using NUnit.Framework;
+using Microsoft.Extensions.Logging;
 
-//namespace UnitTests.Pages
-//{
-//    public class Tests
-//    {
-//        [SetUp]
-//        public void Setup()
-//        {
-//        }
+using NUnit.Framework;
 
-//        [Test]
-//        public void Test1()
-//        {
-//            Assert.Pass();
-//        }
-//    }
-//}
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Privacy
+{
+    /// <summary>
+    /// Unit tests for the Privacy Page.
+    /// </summary>
+    public class PrivacyTests
+    {
+        #region TestSetup
+        // PrivacyModel object.
+        public static PrivacyModel pageModel;
+
+        /// <summary>
+        /// Initialize the mock logger and set up PrivacyModel with PageContext
+        /// and TempData
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<PrivacyModel>>();
+
+            pageModel = new PrivacyModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        /// <summary>
+        /// Test that when OnGet is called, Privacy PageModel returns a
+        /// valid Id. 
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
