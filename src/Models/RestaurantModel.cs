d6cdca57f0872c93fb91186860c54566e15fd44e
benccooper <bcooper@seattleu.edu>
2023-05-05 15:46:35 -0700

    Updated code and documentation in ‘RestaurantModel.cs’ to reflect name change

--- src/Models/RestaurantModel.cs
+++ src/Models/RestaurantModel.cs
@@ -5,26 +5,26 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
-    public class ProductModel
+    public class RestaurantModel
     {
-        // Declare a public property Id of type string to represent the ID of the product.
+        // Declare a public property Id of type string to represent the ID of the restaurant.
         public string Id { get; set; }
-        // Declare a public property Maker of type string to represent the maker of the product.
+        // Declare a public property Maker of type string to represent the maker of the restaurant.
         public string Maker { get; set; }
         // Use the JsonPropertyName attribute to map the Image property to the "img" field in the JSON data.
         [JsonPropertyName("img")]
-        // Declare a public property Image of type string to represent the image URL of the product.   
+        // Declare a public property Image of type string to represent the image URL of the restaurant.   
         public string Image { get; set; }
-        // Declare a public property Url of type string to represent the URL of the product.        
+        // Declare a public property Url of type string to represent the URL of the restaurant.        
         public string Url { get; set; }
-        // Declare a public property Title of type string to represent the title of the product.
+        // Declare a public property Title of type string to represent the title of the restaurant.
         public string Title { get; set; }
-        // Declare a public property Description of type string to represent the description of the product.
+        // Declare a public property Description of type string to represent the description of the restaurant.
         public string Description { get; set; }
-        // Declare a public property Ratings of type int[] to represent the ratings of the product.
+        // Declare a public property Ratings of type int[] to represent the ratings of the restaurant.
         public int[] Ratings { get; set; }
-        // Override the ToString method to serialize the product to a JSON string using the JsonSerializer.
-        public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
+        // Override the ToString method to serialize the restaurant to a JSON string using the JsonSerializer.
+        public override string ToString() => JsonSerializer.Serialize<RestaurantModel>(this);
 
 
     }
