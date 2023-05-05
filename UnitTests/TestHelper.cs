52f76e43a8bcc172a728c3e66a0ccf46fe58960a
benccooper <bcooper@seattleu.edu>
2023-05-05 16:04:10 -0700

    Updated code and documentation in UnitTests/TestHelper.cs to reflect name changes

--- UnitTests/TestHelper.cs
+++ UnitTests/TestHelper.cs
@@ -51,8 +51,8 @@ namespace UnitTests
         // Test PageContext object
         public static PageContext PageContext;
 
-        // Test instance of JsonFileProductService
-        public static JsonFileProductService ProductService;
+        // Test instance of RestaurantService
+        public static RestaurantService _RestaurantService;
 
         /// <summary>
         /// Default Constructor
@@ -92,13 +92,13 @@ namespace UnitTests
             };
 
             // Initialize test service with MockWebHostEnvironment
-            ProductService = new JsonFileProductService(MockWebHostEnvironment.Object);
+            _RestaurantService = new RestaurantService(MockWebHostEnvironment.Object);
 
-            // Declare JsonFileProductService instance
-            JsonFileProductService productService;
+            // Declare RestaurantService instance
+            RestaurantService restaurantService;
 
-            // Initialize test productService with TestHelper.MockWebHostEnvironment
-            productService = new JsonFileProductService(TestHelper.MockWebHostEnvironment.Object);
+            // Initialize test restaurantService with TestHelper.MockWebHostEnvironment
+            restaurantService = new RestaurantService(TestHelper.MockWebHostEnvironment.Object);
         }
     }
 }
