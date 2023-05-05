86e5a2a2efa48d29d3e6e24dfffa6f5188d7fdc5
benccooper <bcooper@seattleu.edu>
2023-05-05 10:18:30 -0700

    Updated mappings and documentation in RestaurantController.cs

--- src/Controllers/RestaurantsController.cs
+++ src/Controllers/RestaurantsController.cs
@@ -2,51 +2,51 @@
 using System.Collections.Generic;
 // Import the Microsoft.AspNetCore.Mvc namespace for creating web applications using the MVC pattern.
 using Microsoft.AspNetCore.Mvc;
-// Import the namespace for the models used by the ProductsController.
+// Import the namespace for the models used by the RestaurantsController.
 using ContosoCrafts.WebSite.Models;
-// Import the namespace for the services used by the ProductsController.
+// Import the namespace for the services used by the RestaurantsController.
 using ContosoCrafts.WebSite.Services;
-//Declare new namespace for the ProductsController..
+//Declare new namespace for the RestaurantsController..
 namespace ContosoCrafts.WebSite.Controllers
 {
     // Indicate that this controller should use the default behavior for API controllers.
     [ApiController]
     // Specify the route template for this controller
     [Route("[controller]")]
-    // Declare the ProductsController class, which is derived from ControllerBase.
-    public class ProductsController : ControllerBase
+    // Declare the RestaurantController class, which is derived from ControllerBase.
+    public class RestaurantsController : ControllerBase
     {
-        // Define the constructor for the ProductsController.
-        public ProductsController(JsonFileProductService productService)
+        // Define the constructor for the RestaurantsController.
+        public RestaurantsController(RestaurantService restaurantService)
         {
-            // Assign the productService parameter to the ProductService property.
-            ProductService = productService;
+            // Assign the restaurantService parameter to the RestaurantService property.
+            RestaurantService = restaurantService;
         }
-        // Declare a public property ProductService of type JsonFileProductService.
-        public JsonFileProductService ProductService { get; }
+        // Declare a public property RestaurantService of type RestaurantService.
+        public RestaurantService RestaurantService { get; }
         // Indicate that this method should handle HTTP GET requests.
         [HttpGet]
         // Define the Get method.
-        public IEnumerable<ProductModel> Get()
+        public IEnumerable<RestaurantModel> Get()
         {
-            // Call the GetProducts method of the ProductService and return the result.
-            return ProductService.GetProducts();
+            // Call the GetRestaurants method of the RestaurantService and return the result.
+            return RestaurantService.GetRestaurants();
         }
         // Indicate that this method should handle HTTP PATCH requests.
         [HttpPatch]
         // Define the Patch method and specify that the request body should be deserialized into a RatingRequest object.
         public ActionResult Patch([FromBody] RatingRequest request)
         {
-            // Call the AddRating method of the ProductService to add the rating to the product.
-            ProductService.AddRating(request.ProductId, request.Rating);
+            // Call the AddRating method of the RestaurantService to add the rating to the restaurant.
+            RestaurantService.AddRating(request.RestaurantId, request.Rating);
             // Return an HTTP 200 OK status code.
             return Ok();
         }
         // Define the RatingRequest class, which has two properties.
         public class RatingRequest
         {
-            // Declare a public property ProductId of type string.
-            public string ProductId { get; set; }
+            // Declare a public property RestaurantId of type string.
+            public string RestaurantId { get; set; }
 
             // Declare a public property Rating of type int.
             public int Rating { get; set; }
