71449144fd9ab044bbff0ae86c29826015e73054
benccooper <bcooper@seattleu.edu>
2023-05-06 22:38:05 -0700

    Updated variable name ‘_RestaurantService’ to ‘restaurantService’ in ‘Delete.cshtml.cs’

--- src/Pages/Restaurant/Delete.cshtml.cs
+++ src/Pages/Restaurant/Delete.cshtml.cs
@@ -17,16 +17,16 @@ namespace ContosoCrafts.WebSite.Pages.Restaurant
         /// <summary>
         /// The "DeleteModel" class is defined, which inherits from the "PageModel" class provided by the Razor Pages framework in ASP.NET Core.
         /// </summary>
-        public RestaurantService _RestaurantService { get; }
+        public RestaurantService restaurantService { get; }
 
         /// <summary>
         /// Defualt Construtor
         /// </summary>
         /// <param name="logger"></param>
-        /// <param name="_RestaurantService"></param>
+        /// <param name="restaurantService"></param>
         public DeleteModel(RestaurantService restaurantService)
         {
-            _RestaurantService = restaurantService;
+            this.restaurantService = restaurantService;
         }
 
         // The data to show, bind to it for the post
@@ -36,7 +36,7 @@ namespace ContosoCrafts.WebSite.Pages.Restaurant
         /// <summary>
         /// The "OnGet" method is defined, which takes a string parameter called "id".
         /// This method is called when the page is loaded with a GET request.
-        /// The method calls the "GetRestaurants" method on the "_RestaurantService" property,
+        /// The method calls the "GetRestaurants" method on the "restaurantService" property,
         /// which returns a list of "RestaurantModel" objects.
         /// The "FirstOrDefault" LINQ method is called on the list to find the first object whose "Id" property equals the "id" parameter.
         /// If a matching object is found, it is assigned to the "Restaurant" property.
@@ -44,13 +44,13 @@ namespace ContosoCrafts.WebSite.Pages.Restaurant
         /// <param name="id"></param>
         public void OnGet(string id)
         {
-            Restaurant = _RestaurantService.GetRestaurants().FirstOrDefault(m => m.Id.Equals(id));
+            Restaurant = restaurantService.GetRestaurants().FirstOrDefault(m => m.Id.Equals(id));
         }
 
         /// <summary>
         /// The "OnPost" method is defined, which is called when the form on the page is submitted with a POST request. The method first checks if the model state is valid.
         /// If it is not, the method returns the current page to show validation errors.
-        /// If the model state is valid, the "DeleteData" method on the "_RestaurantService" property is called with the "Id" property of the "Restaurant" object as the parameter.
+        /// If the model state is valid, the "DeleteData" method on the "restaurantService" property is called with the "Id" property of the "Restaurant" object as the parameter.
         /// This deletes the data for the specified record.
         /// Finally, the method returns a redirect to the Restaurant/Index page of the restaurant data.
         /// </summary>
@@ -62,7 +62,7 @@ namespace ContosoCrafts.WebSite.Pages.Restaurant
                 return Page();
             }
 
-            _RestaurantService.DeleteData(Restaurant.Id);
+            restaurantService.DeleteData(Restaurant.Id);
 
             return RedirectToPage("/Restaurant/Index");
         }
