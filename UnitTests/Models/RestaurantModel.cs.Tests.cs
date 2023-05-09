cc149df5b7d167515401e6dbb2dfffeb05172487
benccooper <bcooper@seattleu.edu>
2023-05-09 12:44:56 -0700

    Added empty RestaurantModel.cs.Tests.cs unit testing file to UnitTests/Models folder

---
+++ UnitTests/Models/RestaurantModel.cs.Tests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class RestaurantModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1() => Assert.Pass();
+    }
+}
