42a02570ecb4a9ac585bb5e58005808be87dfae6
benccooper <bcooper@seattleu.edu>
2023-05-14 12:08:28 -0700

    Updated documentation in Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,26 @@ using Microsoft.Extensions.Logging;
 
 namespace FoodieSeattle.WebSite.Pages
 {
+    /// <summary>
+    /// Privacy Page Model for the Privacy Page.
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        // Logger property
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        ///  Creates an Privacy logger which uses a log category equal to the name of the model.
+        /// </summary>
+        /// <param name="logger">An instance of the built-in ILogger service to use</param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Default OnGet
+        /// </summary>
         public void OnGet()
         {
         }
