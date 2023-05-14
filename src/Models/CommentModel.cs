deed7bcf259da23c3cfac22e76522d5b30510a2c
benccooper <bcooper@seattleu.edu>
2023-05-13 17:39:09 -0700

    Added CommentModel.cs file under FoodieSeattle.Website/Models

---
+++ src/Models/CommentModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodieSeattle.WebSite.Models
+{
+    /// <summary>
+    /// Comments entered by the user about the Restaurant
+    /// </summary>
+    public class CommentModel
+    {
+        // The ID for this comment, use a Guid so it is always unique
+        public string Id { get; set; } = System.Guid.NewGuid().ToString();
+
+        // The Comment
+        public string Comment { get; set; }
+    }
+}
