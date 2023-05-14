4681894e1a7d0ad0813827523a1be247f8b0727a
benccooper <bcooper@seattleu.edu>
2023-05-14 12:26:22 -0700

    Removed commented out code in Pages/Launch/Index.cshtml.cs

--- src/Pages/Launch/Index.cshtml.cs
+++ src/Pages/Launch/Index.cshtml.cs
@@ -1,16 +1,1 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
-
-//namespace FoodieSeattle.WebSite.Pages.Launch
-//{
-//	public class IndexModel : PageModel
-//    {
-//        public void OnGet()
-//        {
-//        }
-//    }
-//}
+﻿
