9c1885e7445015d87ffa6489fbb48dbe8ceb6ca0
BubuCatt <jiayuhsu8240@gmail.com>
2023-05-20 00:49:13 -0700

    Update site.js
    Update site.js function

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your Javascript code.
+
+    // function for dynamic background
+    var images = [
+
+    'landing_page_background_photo.jpg',
+    '2.jpg',
+    '3.jpg',
+    // add more image file names here...
+    ];
+
+    var currentImageIndex = 0;
+
+        function changeBackgroundImage() {
+
+            var dynamicBackground = document.getElementById('dynamic-background');
+
+            if (dynamicBackground) {
+        dynamicBackground.style.backgroundImage = "url('/img/images/" + images[currentImageIndex] + "')";
+                dynamicBackground.style.backgroundSize = "cover"; // for full window background
+
+                currentImageIndex++;
+                if (currentImageIndex == images.length) { // Reset the counter to loop the images
+        currentImageIndex = 0;
+                }
+            }
+
+        }
+
+            window.onload = function () {
+        // Set the initial background
+        changeBackgroundImage();
+
+                // Change the background every 5 seconds
+                setInterval(changeBackgroundImage, 5000);
+            };
+
+
+
+
