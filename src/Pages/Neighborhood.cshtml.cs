f26b0ecfcc55c4e0b99b894a3f7fda8974253510
benccooper <bcooper@seattleu.edu>
2023-05-23 16:45:49 -0700

    Added Neighborhood.cshtml.cs file under src/Pages

---
+++ src/Pages/Neighborhood.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using FoodieSeattle.WebSite.Services;
+using FoodieSeattle.WebSite.Models;
+
+namespace FoodieSeattle.WebSite.Pages
+{
+	public class NeighborhoodModel : PageModel
+    {
+        //
+        public RestaurantService restaurantService { get; }
+
+        // This will hold the selected Neighborhood type
+        public string SelectedNeighborhood { get; set; }
+
+        // This will hold the restaurants of the selected Neighborhood type
+        public IEnumerable<RestaurantModel> Restaurants { get; set; } 
+
+        // logger for NeighborhoodsModel
+        private ILogger<NeighborhoodModel> _logger;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="restaurantService"></param>
+        public NeighborhoodModel(ILogger<NeighborhoodModel> logger, RestaurantService restaurantService)
+        {
+            _logger = logger;
+            this.restaurantService = restaurantService;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Neighborhood"></param>
+        public void OnGet(string Neighborhood)
+        {
+            SelectedNeighborhood = Neighborhood;
+
+            // Fetch the restaurants of the selected Neighborhood type
+            Restaurants = restaurantService.GetRestaurants().Where(r => r.Neighborhood == Neighborhood).ToList();
+        }
+    }
+}
