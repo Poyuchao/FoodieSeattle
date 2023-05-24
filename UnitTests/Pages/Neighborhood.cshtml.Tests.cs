d1de318245e82a748a0640d000ac34ad924ee328
benccooper <bcooper@seattleu.edu>
2023-05-23 21:14:56 -0700

    Uploaded new unit testing file ‘Neighborhood.cshtml.Tests.cs’ under UnitTests/Pages

---
+++ UnitTests/Pages/Neighborhood.cshtml.Tests.cs
@@ -0,0 +1,56 @@
+using FoodieSeattle.WebSite.Pages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
+
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Unit tests for the Neighborhood Page.
+    /// </summary>
+    public class NeighborhoodTests
+    {
+        #region TestSetup
+
+        // NeighborhoodModel object.
+        private static NeighborhoodModel pageModel;
+
+        /// <summary>
+        /// Initialize mock Logger. 
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var mockLoggerDirect = Mock.Of<ILogger<NeighborhoodModel>>();
+
+            pageModel = new NeighborhoodModel(mockLoggerDirect, TestHelper.RestaurantServiceObject);
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+
+        /// <summary>
+        /// Tests that when OnGet function of neighborhoods page 
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_Restaurants()
+        {
+            // Arrange
+            var neighborhood = "Ballard";
+            var expectedCount = 3;
+
+            // Act
+            pageModel.OnGet(neighborhood);
+
+            // Assert
+            Assert.AreEqual(expectedCount, pageModel.Restaurants.Count());
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
+}
