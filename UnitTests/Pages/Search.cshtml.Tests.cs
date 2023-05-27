c7bbba3531d04e440c339cd528d22e2c25d1fc58
benccooper <bcooper@seattleu.edu>
2023-05-27 16:50:48 -0700

    Added unit testing file Search.cshtml.Tests.cs to UnitTests/Pages folder

---
+++ UnitTests/Pages/Search.cshtml.Tests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodieSeattle.WebSite.Pages;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+
+{
+    /// <summary>
+    /// Unit testing for the Search Page
+    /// </summary>
+    public class SearchTests
+    {
+        #region TestingSetup
+
+        // Page model for the Search page
+        public static SearchModel pageModel;
+
+        /// <summary>
+        /// Initializes the pageModel
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new SearchModel(TestHelper.RestaurantServiceObject)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        /// <summary>
+        /// Tests the OnGet function of the Search page, should return requested restaurant
+        /// for a valid search
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Search_Should_Return_RequestId()
+        {
+
+            // Arrange
+            pageModel.Query = "Ba Bar";
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, pageModel.Results.ToList().Any());
+        }
+
+        #endregion OnGet
+    }
+}
